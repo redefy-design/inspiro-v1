bb2f45b2678ef4844e4f0979d048b040c2076713
redefy <redefy.design@gmail.com>
2025-10-30 16:21:46 +0700

    flat folder

--- config/folders.js
+++ config/folders.js
@@ -1,42 +1,10 @@
 export const FOLDERS = [
   {
     name: 'Airbnb',
-    path: '/Users/yourName/Documents/Mobbin Web/Travel/Airbnb web Jun 2025'
+    path: '/Users/yourName/Documents/Airbnb'
   },
   {
-    name: 'Booking',
-    path: '/Users/yourName/Documents/Mobbin Web/Travel/Booking.com web Jun 2023'
-  },
-  {
-    name: 'Dub',
-    path: '/Users/yourName/Documents/Mobbin Web/Finance/Dub web Oct 2025'
-  },
-  {
-    name: 'Expedia',
-    path: '/Users/yourName/Documents/Mobbin Web/Travel/Expedia web Jun 2024'
-  },
-  {
-    name: 'Going',
-    path: '/Users/yourName/Documents/Mobbin Web/Travel/Going web Aug 2023'
-  },
-  {
-    name: 'Kayak',
-    path: '/Users/yourName/Documents/Mobbin Web/Travel/KAYAK web Apr 2025'
-  },
-  {
-    name: 'Mindtrip',
-    path: '/Users/yourName/Documents/Mobbin Web/Travel/Mindtrip web Aug 2025'
-  },
-  {
-    name: 'TravelPerk',
-    path: '/Users/yourName/Documents/Mobbin Web/Travel/TravelPerk web Jun 2025'
-  },
-  {
-    name: 'Trip Advisor',
-    path: '/Users/yourName/Documents/Mobbin Web/Travel/Tripadvisor web Apr 2025'
-  },
-  {
-    name: 'Uber',
-    path: '/Users/yourName/Documents/Mobbin Web/Travel/Uber web Jun 2025'
+    name: 'Booking.com',
+    path: '/Users/yourName/Documents/Booking.com'
   }
 ];
