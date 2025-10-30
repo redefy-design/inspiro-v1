030b597f46b0d01beb07710db9c75f087eb5ee7f
redefy <redefy.design@gmail.com>
2025-10-30 16:17:19 +0700

    updt  folder

--- config/folders.js
+++ config/folders.js
@@ -1,42 +1,42 @@
 export const FOLDERS = [
   {
     name: 'Airbnb',
-    path: '/Users/lynx/Documents/Mobbin Web/Travel/Airbnb web Jun 2025'
+    path: '/Users/yourName/Documents/Mobbin Web/Travel/Airbnb web Jun 2025'
   },
   {
     name: 'Booking',
-    path: '/Users/lynx/Documents/Mobbin Web/Travel/Booking.com web Jun 2023'
+    path: '/Users/yourName/Documents/Mobbin Web/Travel/Booking.com web Jun 2023'
   },
   {
     name: 'Dub',
-    path: '/Users/lynx/Documents/Mobbin Web/Finance/Dub web Oct 2025'
+    path: '/Users/yourName/Documents/Mobbin Web/Finance/Dub web Oct 2025'
   },
   {
     name: 'Expedia',
-    path: '/Users/lynx/Documents/Mobbin Web/Travel/Expedia web Jun 2024'
+    path: '/Users/yourName/Documents/Mobbin Web/Travel/Expedia web Jun 2024'
   },
   {
     name: 'Going',
-    path: '/Users/lynx/Documents/Mobbin Web/Travel/Going web Aug 2023'
+    path: '/Users/yourName/Documents/Mobbin Web/Travel/Going web Aug 2023'
   },
   {
     name: 'Kayak',
-    path: '/Users/lynx/Documents/Mobbin Web/Travel/KAYAK web Apr 2025'
+    path: '/Users/yourName/Documents/Mobbin Web/Travel/KAYAK web Apr 2025'
   },
   {
     name: 'Mindtrip',
-    path: '/Users/lynx/Documents/Mobbin Web/Travel/Mindtrip web Aug 2025'
+    path: '/Users/yourName/Documents/Mobbin Web/Travel/Mindtrip web Aug 2025'
   },
   {
     name: 'TravelPerk',
-    path: '/Users/lynx/Documents/Mobbin Web/Travel/TravelPerk web Jun 2025'
+    path: '/Users/yourName/Documents/Mobbin Web/Travel/TravelPerk web Jun 2025'
   },
   {
     name: 'Trip Advisor',
-    path: '/Users/lynx/Documents/Mobbin Web/Travel/Tripadvisor web Apr 2025'
+    path: '/Users/yourName/Documents/Mobbin Web/Travel/Tripadvisor web Apr 2025'
   },
   {
     name: 'Uber',
-    path: '/Users/lynx/Documents/Mobbin Web/Travel/Uber web Jun 2025'
+    path: '/Users/yourName/Documents/Mobbin Web/Travel/Uber web Jun 2025'
   }
 ];
